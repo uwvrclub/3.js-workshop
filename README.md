d15298fb08d87effd73522ad1865d899554933be
msmel01 <ms.elly17.01@gmail.com>
2021-02-06 17:29:09 +0330

    initial draft of readme + workshop code

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# 3.js-workshop
+# UWVR club workshop: animating a 3D ball with 3.js
+
+Hello&#x1F44B;! In this workshop, we'll be going over some of the basic concepts of the JavaScript API 3.js, which is used to render 3D animated graphics in your web browser.
+
+<!-- add a little more description about 3.js that you will be talking about verbally -->
+
+## What is UWVR?
+UWVR is a student organization at the University of Waterloo dedicated to increasing awareness about careers in the field of Virtual Reality and Augmented Reality, encouraging project development among students, and hosting cool events around new experiences in VR. We started in 2018 as Canada's first ever student VR community, and our current team, consisting of Falah, Brandon, Ayan, Aarushi and myself, carry the same passion that grew the club to where it stands today.
+
+## Prereqs for this workshop
+* Basic html
+* Basic javascript (recommended)
+* Understanding of variables (any language; recommended)
+* Understanding of functions (any language; recommended)
+* Understanding of objects (any language; recommended)
+
+## Setup
+We will be using python3 `http.server` to locally test our web app. It comes installed with python so as long as you have python on your computer, you'll be fine.
+
+<!-- Add instructions to install python and add PATH to environment variables or find a website that does so -->
+
+## Concepts covered in this workshop
+* Renderer
+* Scene
+* Camera
+* Geometry
+* Material
+* Mesh
+* Lighting
+* Simple animation
+
+## What will we be creating?
+
+A cute little bouncing ball &#x26BD;
+
+<!--  insert gif here -->
+
+## Resources
+* Workshop presentation placed in repo
+* [three.js fundamentals](https://threejsfundamentals.org/)
+* You can find lots of cool demos and examples in [threejs.org](https://threejs.org/)
+* Free [udacity 3D graphics course](https://www.udacity.com/course/interactive-3d-graphics--cs291), which teaches general fundamentals of 3D graphics but has all code samples and questions using three.js
+
+<!-- add more resources here -->
+
+<!-- ## Afterword
+
+Great work &#x1F44F; You now have an animated bouncing ball AND you have learned the very fundamentals of creating 3D animation. Most of these concepts can be applied to creating 3D animation using many different programming languages, libraries, and software. 
+
+Of course, you can create so much more than a bouncing ball in three.js.  -->
+
+
---
+++ js/app.js
@@ -0,0 +1,121 @@
+// import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r125/three.module.js'
+
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@v0.108.0/build/three.module.js";
+import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@v0.108.0/examples/jsm/controls/OrbitControls.js";
+
+function main() {
+    const canvas = document.querySelector('#b');
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    
+    // Perspective Camera
+    const fov = 45;
+    const aspect = 1.4;
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 5;
+    camera.position.y = 2;
+    camera.position.x = 0;
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xf0f0f0);
+
+    // Orbit controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    // directional light
+    {
+        const color = 0xFFFFFF;
+        const intensity = 0.7;
+        const directionalLight = new THREE.DirectionalLight(color, intensity);
+        directionalLight.position.set(-1, 2, 4);
+        const light = new THREE.AmbientLight( 0x636363 ); // soft white light
+        scene.add(light);
+        scene.add(directionalLight);
+        light.name = "scene light";
+    }
+
+    // Primitive Geometry shape
+    const radius = 1;
+    const widthSegments = 32;
+    const heightSegments = 32;
+    const ballGeometry = new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments);
+
+    // Material
+    // const ballMaterial = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
+    // const ball = new THREE.Mesh(ballGeometry, ballMaterial);
+    // scene.add(ball);
+
+    // Textures
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('assets/schcbgfp_2K_Albedo.jpg');
+    texture.repeat.set(1, 1);
+    const ballMaterial = new THREE.MeshPhongMaterial({map: texture});
+    const ball = new THREE.Mesh(ballGeometry, ballMaterial);
+    scene.add(ball);
+
+    // plane
+    {
+        const planeSize = 20;
+
+        const planeTexture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
+        planeTexture.wrapS = THREE.RepeatWrapping;
+        planeTexture.wrapT = THREE.RepeatWrapping;
+        planeTexture.magFilter = THREE.NearestFilter;
+        const repeats = planeSize / 2;
+        planeTexture.repeat.set(repeats, repeats);
+
+        const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+        const planeMat = new THREE.MeshPhongMaterial({
+            map: planeTexture,
+            side: THREE.DoubleSide,
+        });
+        const mesh = new THREE.Mesh(planeGeo, planeMat);
+        mesh.rotation.x = Math.PI * -.5;
+        scene.add(mesh);
+    }
+
+    function resizeRendererToDisplaySize(renderer) { // should explain why this works later on
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    // Animation loop
+    function render(time) {
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        time *= 0.001;  // convert time to seconds
+
+        ball.position.y = Math.sin(4*time)+2;
+        
+        // freq = 1.0; //oscillations per second 
+        // amplitude =  90; 
+        // decay =  .5;
+        // posCos = Math.abs(Math.cos(freq*time*2*Math.PI));
+        // y = amplitude*posCos/Math.exp(decay*time);
+        // position - [0,y]
+
+        controls.update();
+        //controls.addEventListener('change', light_update(scene.getObjectByName("scene light")));
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+}
+
+main();
